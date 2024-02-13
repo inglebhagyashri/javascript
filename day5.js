fc82723982da2a9034ca22fef61cd7d20d4b483a
inglebhagyashri <inglebhagyashri31@gmail.com>
2024-02-13 19:22:31 +0530

    added

---
+++ day5.js
@@ -0,0 +1,51 @@
+// date - 13/02/2024
+// loops for while
+// for(inatilaization ; condition ; increament)
+  
+// program 1
+// print 1 to 5
+for (let i = 1 ; i <= 5 ; i++){
+    console.log(i)
+}
+
+// program 2
+//  print 1 to 3
+for(let i = 1 ; i <= 3 ; i++){
+    console.log(i)
+}
+
+// program 3
+//  print table of 2
+for (let i = 2 ; i <= 20 ; i=i+2){
+    console.log(i)
+}
+
+// program 4
+//  print table of 2 in reverse
+
+for(let i = 20 ; i >= 2 ; i = i-2){  // 20 ,18,16,14,------ 0
+    console.log(i) // 20 ,18, 16,14 -----------2
+}
+
+//  program 5
+// print table of 5
+for(let i = 5 ; i <= 50 ; i= i+5){
+    console.log(i)
+}
+
+//  program 6
+//  print table of 5 in reverse
+for(let i= 50 ; i>= 5 ; i= i-5){
+    console.log(i)
+}
+
+//  loops  foe while with break
+
+
+
+
+
+
+
+
+
